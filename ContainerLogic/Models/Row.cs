c138684aefaf200b91f1efd657c58052aead5388
Jurjen Verbruggen <401919@student.fontys.nl>
2019-05-08 10:01:31 +0200

    Added container placement algorithm version 1

--- ContainerLogic/Models/Row.cs
+++ ContainerLogic/Models/Row.cs
@@ -8,21 +8,185 @@ namespace ContainerLogic.Models
 {
     public class Row
     {
-        private List<Stack> stacks;
+        private int pivot;
 
-        public Row(List<Stack> stacks)
+        private List<Stack> stacks;
+        public IReadOnlyList<Stack> Stacks => stacks;
+
+        public Row(int length)
         {
-            this.stacks = stacks;
+            Load(length);
+        }
+
+        private void Load(int length)
+        {
+            pivot = GetPivotIndex();
+            stacks = GetNewStacks(length);
+        }
+
+        private List<Stack> GetNewStacks(int length)
+        {
+            List<Stack> stacks = new List<Stack>();
+
+            for (int i = 0; i < length; i++)
+            {
+                Stack s = new Stack();
+                stacks.Add(s);
+            }
+
+            return stacks;
         }
 
         public int TotalWeight()
         {
             int totalWeight = 0;
-            foreach(Stack s in stacks)
+            foreach (Stack s in stacks)
             {
                 totalWeight += s.TotalWeight();
             }
             return totalWeight;
+        }
+
+        /// <summary>
+        /// Gets the pivot point of row length
+        /// </summary>
+        /// <returns> 
+        /// If width even, returns middle-right index of boat width
+        /// If width uneven, returns middle index of boat width
+        /// </returns>
+        public int GetPivotIndex()
+        {
+            int pivot;
+            double pivotDouble = Stacks.Count / 2;
+            pivot = (int)pivotDouble;
+            //if (pivotDouble % 1 != 0) // even
+            //{
+            //    pivot = ((int)pivotDouble) - 1;
+            //}
+            //else // uneven
+            //{
+            //    pivot = (int)pivotDouble;
+            //}
+            return pivot;
+        }
+
+        /// <summary>
+        /// Get the distribution of weight on a row
+        /// A very inefficient and stupid solution
+        /// </summary>
+        /// <returns>A number between -1 and 1, -1 being left, 1 being right</returns>
+        public double GetWeightDistribution()
+        {
+            int leftWeight = 0;
+            for (int i = 0; i < pivot; i++)
+            {
+                Stack s = stacks[i];
+                int j = i + 1;
+
+                leftWeight += s.TotalWeight();
+            }
+
+            return (double)leftWeight / TotalWeight() * -2 + 1;
+        }
+
+        public Stack GetNextStack(double weightDistribution, IContainer toHold)
+        {
+            Stack stack;
+
+            if (stacks.Count % 2 == 0) // even
+            {
+                stack = GetNextStackEven(weightDistribution, toHold);
+            }
+            else
+            {
+                stack = GetNextStackUneven(weightDistribution, toHold);
+            }
+
+            return stack;
+        }
+
+        private Stack GetNextStackEven(double weightDistribution, IContainer toHold)
+        {
+            Stack stack = null;
+            if (weightDistribution < 0)
+            {
+                for (int i = 0; i < pivot; i++)
+                {
+                    Stack s = stacks[i]; // SAME (dry???)
+                    if (s.CanHoldWeight(toHold))
+                    {
+                        if (stack == null)
+                        {
+                            stack = s;
+                        }
+                        else if (s.TotalWeight() < stack.TotalWeight())
+                        {
+                            stack = s;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = pivot; i < stacks.Count; i++)
+                {
+                    Stack s = stacks[i]; // SAME
+                    if (s.CanHoldWeight(toHold))
+                    {
+                        if (stack == null)
+                        {
+                            stack = s;
+                        }
+                        else if (s.TotalWeight() < stack.TotalWeight())
+                        {
+                            stack = s;
+                        }
+                    }
+                }
+            }
+            return stack;
+        }
+
+        private Stack GetNextStackUneven(double weightDistribution, IContainer toHold)
+        {
+            Stack stack = null;
+            if (weightDistribution < 0)
+            {
+                for (int i = 0; i <= pivot; i++) // the i<=pivot is the only different thing from the even method (dry????)
+                {
+                    Stack s = stacks[i]; // SAME (dry???)
+                    if (s.CanHoldWeight(toHold))
+                    {
+                        if (stack == null)
+                        {
+                            stack = s;
+                        }
+                        else if (s.TotalWeight() < stack.TotalWeight())
+                        {
+                            stack = s;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = pivot; i < stacks.Count; i++)
+                {
+                    Stack s = stacks[i]; // SAME
+                    if (s.CanHoldWeight(toHold))
+                    {
+                        if (stack == null)
+                        {
+                            stack = s;
+                        }
+                        else if (s.TotalWeight() < stack.TotalWeight())
+                        {
+                            stack = s;
+                        }
+                    }
+                }
+            }
+            return stack;
         }
     }
 }
--- ContainerLogic/Models/Ship.cs
+++ ContainerLogic/Models/Ship.cs
@@ -8,49 +8,126 @@ namespace ContainerLogic.Models
     {
         private List<Row> rows;
         private List<IContainer> containers;
+        private int pivot;
 
-        public Ship(List<Row> rows, List<IContainer> containers)
+        public int MaxWeight { get; private set; }
+
+        public Ship(int length, int width, int maxWeight, List<IContainer> containers)
         {
-            this.rows = rows;
             this.containers = containers;
+
+            Init(length, width, maxWeight);
+        }
+
+        public Ship(int length, int width, int maxWeight)
+        {
+            Init(length, width, maxWeight);
         }
 
         private int GetTotalWeight()
         {
             int totalWeight = 0;
-            foreach(IContainer c in containers)
+            foreach (IContainer c in containers)
             {
                 totalWeight += c.TotalWeight;
             }
             return totalWeight;
         }
 
+        private bool CanAddWeight(IContainer container)
+        {
+            int totalWeight = GetTotalWeight();
+            int containerWeight = container.TotalWeight;
+
+            return totalWeight + containerWeight <= MaxWeight;
+        }
+
+        /// <summary>
+        /// Gets the pivot point of boat width
+        /// </summary>
+        /// <returns> 
+        /// If width even, returns middle-left index of boat width
+        /// If width uneven, returns middle index of boat width
+        /// </returns>
+        private int GetPivotIndex()
+        {
+            int pivot = rows[0].GetPivotIndex();
+            return pivot;
+        }
+
         /// <summary>
         /// Get the distribution of weight on a ship
         /// A very inefficient and stupid solution
         /// </summary>
-        /// <returns>A number between 0 and 1, 0 being left, 1 being right</returns>
+        /// <returns>A number between -1 and 1, -1 being left, 1 being right</returns>
         private double GetWeightDistribution()
         {
-            int totalWeight = GetTotalWeight();
-            int leftWeight = 0;
-            int pivot = rows.Count / 2;
-
-            for(int i = 0; i < pivot; i++)
-            {
-                Row r = rows[i];
-                int rWeight = r.TotalWeight();
-
-                leftWeight += (rWeight / totalWeight);
-            }
-            return leftWeight;
+            double weightDistribution = rows[0].GetWeightDistribution();
+            return weightDistribution;
         }
 
-        public void Load()
+        private List<Row> GetNewRows(int length, int width)
         {
-            rows = new List<Row>();
+            List<Row> newRows = new List<Row>();
+            if (length > 0 && length <= 20)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Row row = new Row(width);
+                    newRows.Add(row);
+                }
+            }
+            return newRows;
+        }
 
-            if()
+        private Row GetNextRow()
+        {
+            Row row = null;
+            foreach (Row r in rows)
+            {
+                if (row == null)
+                {
+                    row = r;
+                }
+                else if (r.TotalWeight() < row.TotalWeight())
+                {
+                    row = r;
+                }
+            }
+            return row;
+        }
+
+        public void Init(int length, int width, int maxWeight)
+        {
+            MaxWeight = maxWeight;
+
+            LoadAllContainers(length, width);
+        }
+
+        public bool LoadAllContainers(int length, int width)
+        {
+            bool loaded = true;
+            rows = GetNewRows(length, width);
+            pivot = GetPivotIndex();
+
+            for (int i = 0; i < containers.Count && loaded; i++)
+            {
+                IContainer c = containers[i];
+
+                Row row = GetNextRow();
+                double weightDistribution = GetWeightDistribution();
+                Stack stack = row.GetNextStack(weightDistribution, c);
+                if (stack != null)
+                {
+                    stack.Add(c);
+                }
+                else
+                {
+                    loaded = false;
+                }
+            }
+
+            return loaded;
         }
     }
 }
